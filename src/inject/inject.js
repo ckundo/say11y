df8986da65dce4b239903044054f0a480e27f9cf
Cameron Cundiff <github@ckundo.com>
2018-11-09 15:37:28 -0500

    Update possible search elements

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,13 +3,25 @@ chrome.extension.sendMessage({}, function(response) {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
-      // ----------------------------------------------------------
-      // This part of the script triggers when page is done loading
-      console.log("Hello. This message was sent from scripts/inject.js");
-      // ----------------------------------------------------------
+      var welcomeEl = document.querySelector("h1");
 
-      var welcome = document.querySelector("h1").textContent;
-      chrome.extension.sendMessage({"speak": welcome});
+      if (welcomeEl) {
+        chrome.extension.sendMessage({"speak": welcomeEl.textContent});
+      }
+
+      var linkEls = document.querySelectorAll("a");
+      var headingEls = document.querySelectorAll("h2");
+      var searchEl = document.querySelector("input[type='search']");
+      searchEl = searchEl || document.querySelector("input[id^='search']");
+      searchEl = searchEl || document.querySelector("input[class^='search']");
+      searchEl = searchEl || document.querySelector("input[name^='search']");
+      searchEl = searchEl || document.querySelector("input[value^='search']");
+      searchEl = searchEl || document.querySelector("input[aria-label^='search']");
+      searchEl = searchEl || document.querySelector("input[aria-label^='Search']");
+
+      var search = "search for";
+      var help = "help";
+      var topics = /topics|headings/;
 
       var recognition = new webkitSpeechRecognition();
       recognition.lang = 'en-US';
@@ -17,18 +29,17 @@ chrome.extension.sendMessage({}, function(response) {
       recognition.start();
 
       recognition.onresult = function(event) {
-        var utterance = event.results[0][0].transcript;
-        var query = "search for";
+        var utterance = event.results[event.results.length - 1][0].transcript;
 
-        if (utterance.startsWith(query)) {
-          var search = document.querySelectorAll("[type='search']")[0];
-          search.focus();
-          search.value = utterance.slice(query.length);
-          search.closest("form").submit();
-        };
+        if (utterance.match(search)) {
+          var query = utterance.slice(search.length + 1);
+
+          searchEl.value = query;
+          searchEl.closest("form").submit();
+        } else if (utterance.match(topics)) {
+          chrome.extension.sendMessage({"speak": headings});
+        }
       };
     }
   }, 10);
 });
-
-
